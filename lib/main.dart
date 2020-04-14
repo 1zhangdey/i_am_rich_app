333b665926be67ce1c410308a0d868d62cf5c972
deyizhang <deyizhang2003@gmail.com>
2020-04-13 21:18:03 -0400

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          body: Center(
+              child: Image.network(
+                  "https://is3-ssl.mzstatic.com/image/thumb/Purple127/v4/ce/c6/f4/cec6f467-2c22-dac4-0015-f57a14334167/source/512x512bb.jpg")),
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Text("I am Rich"),
+            centerTitle: true,
+            backgroundColor: Colors.blueGrey[900],
+          ),
+        ),
+      ),
+    );
